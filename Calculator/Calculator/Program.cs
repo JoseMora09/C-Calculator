33edbac5f087d63c6adbf866385b48d378d7259a
Jose Mora <josmo@microsoft.com>
2021-08-24 17:43:27 -0500

    Answer to users inputs

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -9,17 +9,17 @@ namespace Calculator
             Console.WriteLine("Jose's C# Calculator\n\n\n");
 
             ///Variables to store values
-            float number1, number2, answer;
+            float number1= 0, number2= 0, answer= 0;
             string operand;
 
             ///User inputs the values
-            Console.Write("Enter first number-"\n);
+            Console.Write("Enter first number-\n");
             number1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter a operand <+, -, *, />\n");
+            Console.Write("Enter a operand +, -, *, /\n");
             operand = Console.ReadLine();
 
-            Console.Write("Enter second number-"\n);
+            Console.Write("Enter second number-\n");
             number2 = Convert.ToInt32(Console.ReadLine());
 
             ///User decides on which math output to be used
@@ -27,22 +27,28 @@ namespace Calculator
             switch(operand)
             {
                 case "+":
-                    answer = number1 + number2
+                    answer = number1 + number2;
                         break;
 
                 case "-":
-                    answer = number1 - number2
+                    answer = number1 - number2;
                         break;
 
                 case "*":
-                    answer = number1 * number2
+                    answer = number1 * number2;
                         break;
 
                 case "/":
-                    answer = number1 / number2
-                        break;
+                   answer = number1 / number2;
+                  break;
 
             }
+            ///Answer from user inputs
+            Console.WriteLine("Equals = {0}{1}{2} = {3}", number1, operand, number2, answer);
+
+            Console.Write("Enter any key to exit program");
+            Console.ReadKey();
+
 
 
 
